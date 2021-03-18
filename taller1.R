7b85f2937bd7863bb74359d4c53ca9ca58a12ed4
DiegoF <diego.ledesma@correounivalle.edu.co>
2021-03-17 19:28:41 -0500

    Versión 1 taller1.R

---
+++ taller1.R
@@ -0,0 +1,9 @@
+aInterv <- c(100.5, 150.3, 300.8, 410.6, 700.8)
+cInterv <- c(150.3, 300.8, 410.6, 700.8, 1050.5)
+frecAbs <- c(4, 6, 2, 20, 8)
+n <- sum(frecAbs)
+frecRel <- frecAbs / n
+relFrec.acum = cumsum(frecRel)
+
+
+
